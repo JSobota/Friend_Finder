4a6ca1d4a7a01a17ae2b866ee6cc830f6aa28c47
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-07-27 18:27:44 -0400

    final

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,4 +1,3 @@
-// Chosen CSS
   var config = {
     '.chosen-select': {},
     '.chosen-select-deselect': {
@@ -17,9 +16,7 @@
   for (var config {
       $('.chosen-select').chosen(config['.chosen-select']);
     });
-  // Capture the form inputs 
   $("#submit").on("click", function() {
-    // Form validation
     function validateForm() {
       var isValid = true;
       $('.form-control').each(function() {
@@ -32,22 +29,16 @@
       })
       return isValid;
     }
-    // If all required fields are filled
     if (validateForm() == true) {
-      // Create an object for the user's data
       var userData = {
           name: $("#name").val(),
           photo: $("#photo").val(),
           scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(), ]
         }
-        // Grab the URL of the website
       var currentURL = window.location.origin;
-      // AJAX post the data to the friends API. 
       $.post(currentURL + "/api/friends", userData, function(data) {
-        // Grab the result from the AJAX post so that the best match's name and photo are displayed.
         $("#matchName").text(data.name);
         $('#matchImg').attr("src", data.photo);
-        // Show the modal with the best match 
         $("#resultsModal").modal('toggle');
       });
     } else {
