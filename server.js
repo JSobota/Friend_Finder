576f4f35b368bba3a80a92435d64dc0b347930dd
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-07-27 18:29:28 -0400

    small edit to server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-//set up dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-//creates express server and sets up a port
 var app = express(); 
 var port = process.env.PORT || 3000; 
 
-//body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -15,9 +12,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('app/public'));
 
-//router
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-//Listening to the port that was set up
 app.listen(port, () => console.log("Listening on port %s", port));
